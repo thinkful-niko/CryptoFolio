fe92529c2c5c106ee8e3bb7cc57f18737e851818
Nicolas <nkao552@gmail.com>
2018-01-30 17:17:44 -0800

    space space

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb+srv://nicolaskao:Z51qK4uXEf7tZjfV@cluster0-hgwkb.mongodb.net/test';
+    'mongodb://nicolaskao:1234@ds119088.mlab.com:19088/cryptofolio';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
